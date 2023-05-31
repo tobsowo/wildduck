633f6cd1455d82d04e257b80da04e05c84c22b20
Oluwatobi Owolabi <tobsowo@gmail.com>
2023-05-31 13:22:15 +0100

    Update 05_install_packages.sh

--- setup/05_install_packages.sh
+++ setup/05_install_packages.sh
@@ -3,10 +3,12 @@
 OURNAME=05_install_packages.sh
 
 echo -e "\n-- Executing ${ORANGE}${OURNAME}${NC} subscript --"
+# disable postfix
+systemctl disable postfix
 
 # install nginx
 apt-get update
-apt-get -q -y install pwgen git ufw build-essential libssl-dev dnsutils python software-properties-common nginx wget mongodb-org nodejs redis-server clamav clamav-daemon
+apt-get -q -y install gnupg2 pwgen git ufw build-essential libssl-dev dnsutils python software-properties-common nginx wget mongodb-org nodejs redis-server clamav clamav-daemon
 
 # rspamd
 apt-get -q -y --no-install-recommends install rspamd
